6870a1ff64185a174a50eb3e4a4712f0c23a0de2
Stefan Auditor <stefan.auditor@erdfisch.de>
2013-02-24 11:36:59 +0100

    testing phantom and casper

---
+++ tests/casper/screenshots.js
@@ -0,0 +1,22 @@
+var casper = require('casper').create();
+
+casper.start('http://127.0.0.1:9002', function() {
+  // selector capture
+  //console.log(this.captureBase64('png', '#lga'));
+  // clipRect capture
+  //console.log(this.captureBase64('png', {
+  //  top: 0,
+  //  left: 0,
+  //  width: 320,
+  //  height: 200
+  //}));
+  // whole page capture
+  console.log(this.captureBase64('png'));
+});
+
+casper.start('http://127.0.0.1:9002/user', function() {
+  // whole page capture
+  console.log(this.captureBase64('png'));
+});
+
+casper.run();
