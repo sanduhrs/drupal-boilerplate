092a04b76484e4c0f121ea45bff66629140f467c
Stefan Auditor <stefan.auditor@erdfisch.de>
2013-02-24 21:16:46 +0100

    testing phantom and casper

--- tests/casper/screenshots.js
+++ tests/casper/screenshots.js
@@ -1,14 +1,11 @@
 var casper = require('casper').create();
 
 var links = [
-  'http://127.0.0.1:9002/',
-  'http://127.0.0.1:9002/user',
-  'http://127.0.0.1:9002/admin'
+  'http://127.0.0.1:9002/'
 ];
 
 casper.start().each(links, function(self, link) {
   self.thenOpen(link, function() {
-    console.log("<img src='http://erdfisch.de/sites/all/themes/zen_ninesixty/logo.png' />);
     console.log(this.captureBase64('png'));
   });
 });
